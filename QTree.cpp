7ebcd633dc8c05b34fff0ac2de86572a8a4fe50b
xinzhilinger <58837333+xinzhilinger@users.noreply.github.com>
2022-02-08 11:21:22 +0800

    Delete QTree.cpp

--- QTree.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "QTree.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, QTree, "QTree" );
